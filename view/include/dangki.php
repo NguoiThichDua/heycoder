39675f6e1036c3245edd79b1a7ad1898b5a2019b
Luannguyen1701 <54973629+Luannguyen1701@users.noreply.github.com>
2019-09-07 20:01:34 +0700

    Luân
    -thêm patten trên phần tttk

--- view/include/dangki.php
+++ view/include/dangki.php
@@ -6,15 +6,16 @@
                 <h3>Thông tin tài khoản: </h3>
                 <div class="form-group">
                     <label for="exampleInputEmail1">Email: </label>
-                    <input type="email" class="form-control" aria-describedby="emailHelp" placeholder="Email">
+                    <input type="email" class="form-control" aria-describedby="emailHelp" placeholder="Email"required pattern="^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$" title="Email không hợp lệ.">
                 </div>
                 <div class="form-group">
                     <label for="exampleInputPassword1">Mật khẩu: </label>
-                    <input type="password" class="form-control" placeholder="Mật khẩu">
+                    <input type="password" class="form-control" placeholder="Mật khẩu" required pattern="[a-z]{8,15}"title="Tối thiểu 8 và tối đa 15 kí tự và viết thường toàn bộ.">
                 </div>
                 <div class="form-group">
                     <label for="exampleInputPassword1">Nhập lại mật khẩu: </label>
                     <input type="password" class="form-control" placeholder="Mật khẩu nhập lại">
+                    
                 </div>
             </div>
 
